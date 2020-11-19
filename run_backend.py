6ef9cd9ed4f83ec20d81db2a61b9446e5ec225e3
demidos-snz <sasha-demidkin@ya.ru>
2020-11-19 21:39:23 +0300

    second step

--- run_backend.py
+++ run_backend.py
@@ -1,5 +1,10 @@
-from backend.main import Worker
-from backend.models import Users
+try:
+    from backend.main import Worker
+    from backend.models import Users
+except ImportError:
+    exit('copy settings.py.default->settings.py and '
+         'set TOKEN, TOKEN_MAP and SECRET_KEY_SERVER')
+
 
 if __name__ == '__main__':
     u = Users()
